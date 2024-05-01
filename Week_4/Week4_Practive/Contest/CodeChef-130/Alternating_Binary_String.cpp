9074711fe8a70d95ea90835c7e69fff4e0209db9
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2024-05-01 18:03:54 +0600

    Daily Solution Problem

---
+++ Week_4/Week4_Practive/Contest/CodeChef-130/Alternating_Binary_String.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define vl vector<ll> 
+#define vi vector<int> 
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range_back(i,s,n) for(int i=n ; i >= s; i--)
+#define take_array(v,n) in_range(i,0,n) cin>>v[i]
+#define print_array(v) for(auto c:v){cout<<c<<" ";}cout<<ln
+#define erase_duplicates(x) x.erase(unique(all(x)),x.end());
+#define cin2(a, b) cin >> a >> b;
+#define cin3(a, b, c) cin >> a >> b >> c;
+#define all(x) x.begin(),x.end()
+#define all_0(x) memset(x,0,sizeof(x))
+#define isEven(x) ((x&1)== 0)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define enter cout << "\n"
+#define ln '\n'
+#define PQ priority_queue
+#define Hashmap unordered_map
+#define pb push_back
+#define gcC int main()
+#define checkmate return 0;
+ 
+using namespace std;
+// using namespace __gnu_pbds;
+gcC
+{
+  /* _Coder   : anmamun0
+     _Created : 17 April 2024 ||  20:48:56
+     _File    : Alternating_Binary_String.cpp 
+ 
+    بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;      string s;       cin2(n, s);
+        int cnt = 0;
+        for (int i = 1; i<n; i++)
+            if(s[i-1] == s[i]) cnt++;
+    
+
+        cout << cnt << endl;
+     }
+
+    checkmate;
+}
---
+++ Week_4/Week4_Practive/Contest/CodeChef-130/Good_Binary_String.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define vl vector<ll> 
+#define vi vector<int> 
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range_back(i,s,n) for(int i=n ; i >= s; i--)
+#define take_array(v,n) in_range(i,0,n) cin>>v[i]
+#define print_array(v) for(auto c:v){cout<<c<<" ";}cout<<ln
+#define erase_duplicates(x) x.erase(unique(all(x)),x.end());
+#define cin2(a, b) cin >> a >> b;
+#define cin3(a, b, c) cin >> a >> b >> c;
+#define all(x) x.begin(),x.end()
+#define all_0(x) memset(x,0,sizeof(x))
+#define isEven(x) ((x&1)== 0)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define enter cout << "\n"
+#define ln '\n'
+#define PQ priority_queue
+#define Hashmap unordered_map
+#define pb push_back
+#define gcC int main()
+#define checkmate return 0;
+ 
+using namespace std;
+// using namespace __gnu_pbds;
+gcC
+{
+  /* _Coder   : anmamun0
+     _Created : 01 May 2024 ||  17:42:56
+     _File    : Good_Binary_String.cpp 
+ 
+    بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;  cin>> t;
+    while(t--)
+    {
+        int n;  string s;   cin2(n, s);
+
+        vi idx;
+        char prev = '1';
+
+        for (int i = 0; i < n-1; i+=2)
+        {
+            if(s[i] != s[i+1])
+            {
+                if(prev != s[i])
+                {
+                    idx.pb(i);
+                    prev = s[i];
+                }   
+                else
+                {
+                    idx.pb(i + 1);
+                    prev = s[i + 1];
+                }
+            }
+        }
+
+        cout << idx.size() << "\n";
+        for(auto c:idx)cout<<c+1<<" ";
+        enter;
+    }
+
+
+    checkmate;
+}
