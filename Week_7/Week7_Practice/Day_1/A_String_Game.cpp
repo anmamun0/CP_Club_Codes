83e68d54910aa220d335659dc843e86975f0ad29
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2024-05-12 19:48:53 +0600

    Week 7 Day-1 Problem Solution
    Week 7 Day-1 Problem Solution

---
+++ Week_7/Week7_Practice/Day_1/A_String_Game.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define vl vector<ll> 
+#define vi vector<int> 
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range_back(i,s,n) for(int i=n ; i >= s; i--)
+#define take_array(v,n) in_range(i,0,n) cin>>v[i]
+#define print_array(v) for(auto c:v){cout<<c<<" ";}cout<<ln
+#define erase_duplicates(x) x.erase(unique(all(x)),x.end());
+#define cin2(a, b) cin >> a >> b;
+#define cin3(a, b, c) cin >> a >> b >> c;
+#define all(x) x.begin(),x.end()
+#define all_0(x) memset(x,0,sizeof(x))
+#define isEven(x) ((x&1)== 0)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define enter cout << "\n"
+#define ln '\n'
+#define PQ priority_queue
+#define Hashmap unordered_map
+#define pb push_back
+#define gcC int main()
+#define checkmate return 0;
+ 
+using namespace std;
+// using namespace __gnu_pbds;
+gcC
+{
+  /* _Coder   : anmamun0
+     _Created : 12 May 2024 ||  18:01:00
+     _File    : A_String_Game.cpp 
+ 
+    بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;  cin>> t;
+    while(t--)
+    {
+        int n; cin>> n;
+        string s;   cin >> s;
+
+        stack<int> st;
+        int cnt = 0;
+        for(auto c:s)
+        {
+            if(!st.empty() and st.top() != (c-'0'))
+            {
+                st.pop();
+                cnt++;
+            }
+            else st.push((c - '0'));
+        }
+        cnt %= 2;
+        (cnt == 1) ? cout << "Zlatan\n" : cout << "Ramos\n";
+    }
+
+    checkmate;
+}
---
+++ Week_7/Week7_Practice/Day_1/C_Array_Decrements.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define vl vector<ll> 
+#define vi vector<int> 
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range_back(i,s,n) for(int i=n ; i >= s; i--)
+#define take_array(v,n) in_range(i,0,n) cin>>v[i]
+#define print_array(v) for(auto c:v){cout<<c<<" ";}cout<<ln
+#define erase_duplicates(x) x.erase(unique(all(x)),x.end());
+#define cin2(a, b) cin >> a >> b;
+#define cin3(a, b, c) cin >> a >> b >> c;
+#define all(x) x.begin(),x.end()
+#define all_0(x) memset(x,0,sizeof(x))
+#define isEven(x) ((x&1)== 0)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define enter cout << "\n"
+#define ln '\n'
+#define PQ priority_queue
+#define Hashmap unordered_map
+#define pb push_back
+#define gcC int main()
+#define checkmate return 0;
+ 
+using namespace std;
+// using namespace __gnu_pbds;
+gcC
+{
+  /* _Coder   : anmamun0
+     _Created : 12 May 2024 ||  19:22:11
+     _File    : C_Array_Decrements.cpp 
+ 
+    بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;      cin >> n;
+        vl op(n);   take_array(op,n-1);
+        vl org(n);  take_array(org,n-1);
+
+        ll mx_need = 0;
+        for (int i = 0; i < n;i++)
+        {
+            mx_need = max(mx_need, (op[i] - org[i]));
+        } 
+
+        int flag = true;
+        for (int i = 0; i < n;i++)
+        {
+            int val = op[i] - mx_need;
+            if( !(val<=0 and org[i]==0 ) and !(val==org[i])) flag= false;
+        }
+
+        (flag) ? yes : no;
+    }
+
+    checkmate;
+}
