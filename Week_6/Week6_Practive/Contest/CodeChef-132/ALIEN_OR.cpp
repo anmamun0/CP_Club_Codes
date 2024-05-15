db132e7c8d4884546565772ec61d9fabbd8f63ca
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2024-05-16 01:20:49 +0600

    Aline_OR, Can I make 1 to k num from binary digit?
    Aline_OR, Can I make 1 to k num from binary digit?

---
+++ Week_6/Week6_Practive/Contest/CodeChef-132/ALIEN_OR.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define vl vector<ll> 
+#define vi vector<int> 
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range_back(i,s,n) for(int i=n ; i >= s; i--)
+#define take_array(v,n) in_range(i,0,n) cin>>v[i]
+#define print_array(v) for(auto c:v){cout<<c<<" ";}cout<<ln
+#define erase_duplicates(x) x.erase(unique(all(x)),x.end());
+#define cin2(a, b) cin >> a >> b;
+#define cin3(a, b, c) cin >> a >> b >> c;
+#define all(x) x.begin(),x.end()
+#define all_0(x) memset(x,0,sizeof(x))
+#define isEven(x) ((x&1)== 0)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define enter cout << "\n"
+#define ln '\n'
+#define PQ priority_queue
+#define Hashmap unordered_map
+#define pb push_back
+#define gcC int main()
+#define checkmate return 0;
+ 
+using namespace std;
+// using namespace __gnu_pbds;
+ 
+gcC
+{
+  /* _Coder   : anmamun0
+     _Created : 01 May 2024 ||  21:03:28
+     _File    : ALIEN_OR.cpp 
+ 
+    بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;      cin >> t;
+    while (t--)
+    {
+        int n, k; cin2(n, k);
+
+        vector<int> bit (110);
+        for (int i = 0; i<n; i++)
+        {
+            string s;   cin >> s;
+
+            int pos = 0, cnt = count(all(s), '1');
+            if(cnt==1)
+            {
+                for (int j = k - 1; j >= 0; j--)
+                {
+                    if(s[j] == '1')
+                    {
+                        bit[pos]=1; break;
+                    } else pos++;
+                }
+            }
+        }
+
+        bool ok = true;
+        for (int i = 0; i<k; i++)
+        {
+            if(bit[i]==0){
+                ok = false;  break;
+            }
+        }
+
+        (ok) ? yes : no;
+    }
+
+    checkmate;
+}
