047eff266f25a1467e9a0ce1fa1775fb1712b0f7
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2024-05-23 23:19:49 +0600

    Week 7, Day-3

---
+++ Week_7/Week7_Practice/Day_3/F_Two_Arrays.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define vl vector<ll> 
+#define vi vector<int> 
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range_back(i,s,n) for(int i=n ; i >= s; i--)
+#define take_array(v,n) in_range(i,0,n) cin>>v[i]
+#define print_array(v) for(auto c:v){cout<<c<<" ";}cout<<ln
+#define erase_duplicates(x) x.erase(unique(all(x)),x.end());
+#define cin2(a, b) cin >> a >> b;
+#define cin3(a, b, c) cin >> a >> b >> c;
+#define all(x) x.begin(),x.end()
+#define all_0(x) memset(x,0,sizeof(x))
+#define isEven(x) ((x&1)== 0)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define enter cout << "\n"
+#define ln '\n'
+#define PQ priority_queue
+#define Hashmap unordered_map
+#define pb push_back
+#define gcC int main()
+#define checkmate return 0;
+ 
+using namespace std;
+// using namespace __gnu_pbds;
+gcC
+{
+  /* _Coder   : anmamun0
+     _Created : 23 May 2024 ||  21:10:49
+     _File    : F_Two_Arrays.cpp 
+ 
+    بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;                  cin >> T;
+
+    while(T--)
+    {
+        int n;                  cin >> n;
+        vi a(n), b(n);
+        take_array(a, n - 1);   take_array(b, n - 1);
+        sort(all(a));           sort(all(b));
+
+        int Flag = true;
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if(a[i] > b[i] or (b[i]-a[i]) > 1)
+            {
+                Flag = false;
+                break;
+            }
+        }
+        (Flag) ? yes : no;
+    }
+
+    checkmate;
+}
