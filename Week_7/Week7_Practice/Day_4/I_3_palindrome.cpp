5623ee5308af2460bf0dcdecfae0173e73b73aa2
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2024-05-27 15:49:00 +0600

    Week 7, day 4

---
+++ Week_7/Week7_Practice/Day_4/I_3_palindrome.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define vl vector<ll> 
+#define vi vector<int> 
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range_back(i,s,n) for(int i=n ; i >= s; i--)
+#define take_array(v,n) in_range(i,0,n) cin>>v[i]
+#define print_array(v) for(auto c:v){cout<<c<<" ";}cout<<ln
+#define erase_duplicates(x) x.erase(unique(all(x)),x.end());
+#define cin2(a, b) cin >> a >> b;
+#define cin3(a, b, c) cin >> a >> b >> c;
+#define all(x) x.begin(),x.end()
+#define all_0(x) memset(x,0,sizeof(x))
+#define isEven(x) ((x&1)== 0)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define enter cout << "\n"
+#define ln '\n'
+#define PQ priority_queue
+#define Hashmap unordered_map
+#define pb push_back
+#define gcC int main()
+#define checkmate return 0;
+ 
+using namespace std;
+// using namespace __gnu_pbds;
+gcC
+{
+  /* _Coder   : anmamun0
+     _Created : 27 May 2024 ||  13:25:49
+     _File    : I_3_palindrome.cpp 
+ 
+    بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n; cin >> n;
+
+    string s = ""; 
+
+    int t = floor(n*1.0 / 2); 
+
+    for (int i = 0; i < t; i++)
+    {
+        if(i%2==0)s+="aa";
+        else s+="bb"; 
+    }
+     if(n%2)
+     {
+        if(s=="")s+='a';
+        else if(s.back()=='a')s+='b';
+        else s+='a';
+     }
+    cout << s << endl;
+
+    checkmate;
+}
