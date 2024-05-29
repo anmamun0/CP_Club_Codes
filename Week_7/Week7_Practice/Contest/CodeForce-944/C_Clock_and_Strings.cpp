112c975b797abb853472df7a3939765ac9a3f7b6
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2024-05-29 16:35:55 +0600

    week 7, codeforce 944 contest

---
+++ Week_7/Week7_Practice/Contest/CodeForce-944/C_Clock_and_Strings.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define vl vector<ll> 
+#define vi vector<int> 
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range_back(i,s,n) for(int i=n ; i >= s; i--)
+#define take_array(v,n) in_range(i,0,n) cin>>v[i]
+#define print_array(v) for(auto c:v){cout<<c<<" ";}cout<<ln
+#define erase_duplicates(x) x.erase(unique(all(x)),x.end());
+#define cin2(a, b) cin >> a >> b;
+#define cin3(a, b, c) cin >> a >> b >> c;
+#define all(x) x.begin(),x.end()
+#define all_0(x) memset(x,0,sizeof(x))
+#define isEven(x) ((x&1)== 0)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define enter cout << "\n"
+#define ln '\n'
+#define PQ priority_queue
+#define Hashmap unordered_map
+#define pb push_back
+#define gcC int main()
+#define checkmate return 0;
+ 
+using namespace std;
+// using namespace __gnu_pbds;
+void anmamun0()
+{
+    int a, b, c, d; cin >> a >> b >> c >> d;
+
+   
+    if (a > b)  swap(a, b); if (c > d)  swap(c, d);
+
+    if ((a < c and c < b and b < d)) yes;
+    else if((a > c and a < d and d < b))yes; 
+    else no;
+}
+gcC
+{
+  /* _Coder   : anmamun0
+     _Created : 10 May 2024 ||  21:38:04
+     _File    : C_Clock_and_Strings.cpp 
+ 
+    بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        anmamun0();
+    }
+
+    checkmate;
+}
