96c7ea6c4ce571f07912e9808503382cd12493fe
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2024-06-06 23:49:49 +0600

    Week_9, Day__2

---
+++ Week_9/Week9_Practive/Day_2/K_Subarray_Divisibility.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define vl vector<ll> 
+#define vi vector<int> 
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range(i,s,n) for(int i=s; i<=n; i++)
+#define in_range_back(i,s,n) for(int i=n ; i >= s; i--)
+#define take_array(v,n) in_range(i,0,n) cin>>v[i]
+#define print_array(v) for(auto c:v){cout<<c<<" ";}cout<<ln
+#define erase_duplicates(x) x.erase(unique(all(x)),x.end());
+#define cin2(a, b) cin >> a >> b;
+#define cin3(a, b, c) cin >> a >> b >> c;
+#define all(x) x.begin(),x.end()
+#define all_0(x) memset(x,0,sizeof(x))
+#define isEven(x) ((x&1)== 0)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define enter cout << "\n"
+#define ln '\n'
+#define PQ priority_queue
+#define Hashmap unordered_map
+#define pb push_back
+#define gcC int main()
+#define checkmate return 0;
+ 
+using namespace std;
+// using namespace __gnu_pbds;
+gcC
+{
+  /* _Coder   : anmamun0
+     _Created : 06 June 2024 ||  18:14:25
+     _File    : K_Subarray_Divisibility.cpp 
+ 
+    بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n;  cin >> n;
+    vl v(n);    take_array(v,n-1);
+
+    map<ll, int> mp;
+    mp[0] = 1;
+
+    ll window = 0, cnt = 0,r = 0;
+
+    while(r<n)
+    {
+        window += v[r];
+        int remind = (window % n + n) % n;
+        if(mp.find(remind) != mp.end()) cnt = mp[remind]; 
+        mp[remind]++;
+        r++;
+    }
+    cout << cnt << "\n";
+
+    checkmate;
+}
