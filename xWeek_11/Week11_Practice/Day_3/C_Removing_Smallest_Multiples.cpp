1d15d47a4f9217d4a06f02986fde05982f852fe7
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2024-07-07 23:22:14 +0600

    Week_11_Day_3

---
+++ xWeek_11/Week11_Practice/Day_3/C_Removing_Smallest_Multiples.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T> 
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+typedef long long ll;
+typedef pair<int, int> couple;
+#define str string
+#define X first
+#define Y second
+#define pb push_back
+#define isEven(x) ((x&1)== 0)
+#define isPlus(x) (x>=0)
+#define all(x) x.begin(),x.end() 
+#define len(x) (int) (x).size()
+#define Public int T; input(T);while(T--)
+#define checkmate return  
+#define Run(i, x, y) for (int _s=x, _e=y, _dir=(y>x)?1:-1, i=_s;((_dir==1)?(i<=_e):(i>=_e)); i+=_dir)
+template <typename... DataIn> void input(DataIn&... val){(cin >> ... >> val);}
+template <typename... DataOut> void print(const DataOut&... val){((cout << val), ...);} 
+template <typename T> void input(vector<T>& v){for(auto &c:v)cin>>c;}
+template <typename T> void print(vector<T>& v){for(auto c:v)cout<<c<<" ";}
+namespace AN{void yes() { print("YES\n");} void no(){print("NO\n");} void Enter() {print("\n");} char ln='\n';
+    template<typename Q> inline void Erase_duplicate(vector<Q>& v) { sort(all(v)); v.erase(unique(all(v)),v.end());}}
+using namespace AN;
+
+int main()
+{
+  /* _Coder   : anmamun0
+     _Created : 06 July 2024 ||  23:00:05
+     _File    : C_Removing_Smallest_Multiples.cpp 
+    بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Public
+    {
+        ll n; input(n);
+        str s; input(s);  
+
+        vector<int> v(n+5,0);
+        Run(i, 1, n) if (s[i-1] == '1') v[i] = 100; 
+
+        ll put = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = i; j <= n; j+=i)
+            {
+               if(v[j] == 100) break;  
+                if(!v[j])
+                {
+                    v[j] = 1;
+                    put += i;
+                }
+            }
+        } 
+        print(put, ln);
+    }
+
+    checkmate 0;
+}
