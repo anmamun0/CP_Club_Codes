724667bb0a332e8fbc042569f88f5971c241244e
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2024-08-26 23:58:15 +0600

    week_14_day3

---
+++ xWeek_14/Week14_Practice/Day_3/E_Two_Movies.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T> 
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+typedef long long ll;
+typedef pair<int, int> couple;
+#define  str string
+#define  X first
+#define  Y second
+#define  pb push_back
+#define  all(x) x.begin(),x.end() 
+#define  Public int T; input(T);while(T--)
+#define  checkmate return  
+#define  __stop print('\n');
+#define Run(i, x, y) for (int _s=x, _e=y, _in=(y>x)?1:-1, i=_s;((_in==1)?(i<=_e):(i>=_e)); i+=_in)
+template <typename... DataIn> void input(DataIn&... val){(cin >> ... >> val);}
+template <typename... DataOut> void print(const DataOut&... val){((cout << val), ...);} 
+namespace AN{inline void yes(){cout<<"YES\n";}void no(){cout<<"NO\n";}bool isEven(auto x){return (x&1==0);} bool isPlus(auto x){return (x>=0);} 
+    int len(auto x){return (int)(x).size();} template <typename T> void input(vector<T> &v) { for (auto &c : v) cin >> c; } 
+    template <typename T> void print(vector<T> v){ for(auto c:v)cout<<c<<" ";}char ln = '\n';}
+using namespace AN;
+
+int main()
+{
+  /* _Coder   : anmamun0
+     _Created : 26 August 2024 ||  00:48:37
+     _File    : E_Two_Movies.cpp 
+    بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Public
+    {
+        int n; input(n);
+        vector<int> v(n); input(v);
+        vector<int> u(n); input(u);
+
+        int x = 0, y = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if(v[i] != u[i])
+            {
+                if(v[i]>u[i]) x += v[i];
+                else y += u[i];
+            }
+        }
+        for (int i = 0; i<n; i++)
+        {
+            if(v[i]==u[i])
+            {
+                if(v[i]==1) (x < y)? x++ : y++;
+                else if(v[i]==-1) (x<y)? y-- : x--;
+            }  
+        }
+
+        cout << min(x, y) << endl;
+    }
+
+    checkmate 0;
+}
