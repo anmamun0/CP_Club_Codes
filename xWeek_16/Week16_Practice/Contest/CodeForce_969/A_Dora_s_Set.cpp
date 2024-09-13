728717fd5e4995a5557ec83e421c51ef2090de9e
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2024-09-13 21:28:00 +0600

    Contest_CodeForce_969

---
+++ xWeek_16/Week16_Practice/Contest/CodeForce_969/A_Dora_s_Set.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T> 
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+typedef long long ll;
+typedef pair<int, int> couple;
+#define  str string
+#define  X first
+#define  Y second
+#define  pb push_back
+#define  all(x) x.begin(),x.end() 
+#define  testCase int T; input(T);while(T--)
+#define  checkmate return  
+#define  __stop print('\n');
+#define Run(i, x, y) for (int _s=x, _e=y, _in=(y>x)?1:-1, i=_s;((_in==1)?(i<=_e):(i>=_e)); i+=_in)
+template <typename... DataIn> void input(DataIn&... val){(cin >> ... >> val);}
+template <typename... DataOut> void print(const DataOut&... val){((cout << val), ...);} 
+namespace AN{inline void yes(){cout<<"YES\n";}void no(){cout<<"NO\n";}bool isEven(auto x){return ((x&1)==0);} bool isPlus(auto x){return (x>=0);} 
+    int len(auto x){return (int)(x).size();} template <typename T> void input(vector<T> &v) { for (auto &c : v) cin >> c; } 
+    template <typename T> void print(vector<T> v){ for(auto c:v)cout<<c<<" ";}char ln = '\n';}
+using namespace AN;
+ 
+void root()
+{
+    int x, y; input(x,y);
+
+    if(isEven(x))x++;
+
+    ll result = (y-x+2)/4;
+    cout << result << ln;
+}
+int main()
+{
+  /* _Coder   : anmamun0
+     _Created : 30 August 2024 ||  20:41:55
+     _File    : A_Dora_s_Set.cpp 
+    بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    testCase root();
+
+    checkmate 0;
+}
